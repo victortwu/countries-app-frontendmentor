78375a81a22d5021df5f5ec43a658117d4b86cc2
victortwu <victor607@gmail.com>
2021-09-14 20:31:02 -0700

    basic dark mode working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
 const App = () => {
 
+
+const [darkMode, setDarkMode] = useState(false)
+const [data, setData] = useState([])
+
+const toggleTheme = darkMode ? 'darkMode' : 'lightMode'
+const toggleCardStyle = darkMode ? 'darkCard' : 'lightCard'
+const toggleHeader = darkMode ? 'darkHeader' : 'lightHeader'
 let recs = []
 
-for(let i = 0; i < 8; i++) {
-  recs.push(<div key={i} className='card'/>)
+const getRecs = () => {
+  for(let i = 0; i < 8; i++) {
+    recs.push(<div key={i} className='cardDiv'/>)
+  }
 }
 
+useEffect(() => {
+  getRecs()
+  setData(recs)
+}, [])
+
+
+console.log(data)
 
   return (
-    <div className="App">
-      <header>
+    <div className={toggleTheme}>
+      <header className={toggleHeader}>
         <span>Where in the world?</span>
-        <span>Dark Mode</span>
+        <span onClick={()=> setDarkMode(!darkMode)}>{
+          darkMode ? 'Light Mode' : 'Dark Mode'
+        }</span>
       </header>
 
 
@@ -28,11 +46,15 @@ for(let i = 0; i < 8; i++) {
                   Filter By Region
                 </div>
             </div>
-            <div className='cardsContainer'>
-            {
-              recs.map(elem => {return elem} )
-            }
-            </div>
+              <div className='cardsContainer'>
+
+                {
+                  data.map(elem => {
+                    return <div className={toggleCardStyle}>{elem}</div>
+                  } )
+                }
+
+              </div>
         </main>
     </div>
   );
